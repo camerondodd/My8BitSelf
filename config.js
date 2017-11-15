8d0ef9f81c2143a07e70765adbae5e11931bb264
Cameron Dodd <cameron.dodd@mavs.uta.edu>
2017-11-15 15:06:05 -0500

    checking variables

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                       // 'mongodb://localhost/capstone2';
-                      'mongodb://user:password@ds243285.mlab.com:43285/capstone2';
+                      // 'mongodb://user:password@ds243285.mlab.com:43285/capstone2';
 exports.PORT = process.env.PORT || 8080;
 
 exports.clientID = process.env.clientID ||
