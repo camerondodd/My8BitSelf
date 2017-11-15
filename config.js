b361418dbc83a4365bb4255d2638db28963a64bc
Cameron Dodd <cameron.dodd@mavs.uta.edu>
2017-11-15 15:14:12 -0500

    more variable tweeking

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
+// exports.DATABASE_URL = process.env.DATABASE_URL ||
+//                        global.DATABASE_URL ||
                       // 'mongodb://localhost/capstone2';
                       // 'mongodb://user:password@ds243285.mlab.com:43285/capstone2';
 exports.PORT = process.env.PORT || 8080;
